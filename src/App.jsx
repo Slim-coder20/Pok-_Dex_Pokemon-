6fdd411c3a07d7045d5917122f7ef61bd2d11102
slim <slimabida21@gmail.com>
2025-08-04 21:20:34 +0200

    Mise en place de l'ajout de pokemon via le boutton ajouter avec l'ajout de condition dans la fonction fetchPokemons et la modification du state pokemons en ajoutant le spread operateur

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import Pokecard from "./components/Pokecard/Pokecard";
 export default function App() {
   // State
   const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // Récupérer les 30 premiers Pokemon
-  const fetchPokemons = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const fetchPokemons = async (add = false) => {
+    setLoading(true);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=30${
+        add && "&offset=" + pokemons.length
+      }`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     const data = await response.json();
     // Pour chaque Pokemon, récupérer les détails :
@@ -30,15 +37,14 @@ export default function App() {
       return await response.json();
     });
     const pokemonDetails = await Promise.all(promises);
-    console.log(pokemonDetails);
-    setPokemons(pokemonDetails); // Mettre à jour l'état
+    setPokemons([...pokemons, ...pokemonDetails]); // Mettre à jour l'état
+    setLoading(false);
   };
-  fetchPokemons(); 
 
-//   // Appeler fetchPokemons au chargement du composant
-//   useEffect(() => {
-//     fetchPokemons();
-//   }, []);
+  // Appeler fetchPokemons au chargement du composant
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
 
   return (
     <div>
@@ -50,6 +56,22 @@ export default function App() {
           {pokemons.map((pokemon, index) => (
             <Pokecard key={index} pokemon={pokemon} />
           ))}
+        </div>
+        {/* Loading */}
+        {loading && (
+          <div className="flex justify-center text-white mt-5">
+            chargement...
+          </div>
+        )}
+
+        {/* ajout de nouveaux pokemon */}
+        <div className="flex justify-center my-10">
+          <button
+            className="bg-white hover:bg-yellow-200 rounded-full text-black py-2 px-5 text-lg font-semibold shadow-lg hover:shadow-xl transition duration-150"
+            onClick={() => fetchPokemons(true)}
+          >
+            Encore plus de pokemon
+          </button>
         </div>
       </div>
     </div>
